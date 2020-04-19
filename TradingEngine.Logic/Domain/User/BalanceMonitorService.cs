69729b0065553d0910a35373efb9cc8660552b24
Arnel Ambrosio <arnelfambrosio@gmail.com>
2020-04-19 16:18:36 +0800

    -fixed and updates

---
+++ TradingEngine.Logic/Domain/User/BalanceMonitorService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingEngine.Logic.Domain.User
+{
+    public class BalanceMonitorService : IBalanceMonitorService
+    {
+        public Task<bool> ChargeCeiling(User userToMonitor, Currency currencyToMonitor, decimal ceilingAmount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TradingEngine.Logic/Domain/User/IBalanceMonitorService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingEngine.Logic.Domain.User
+{
+    public interface IBalanceMonitorService
+    {
+        Task<bool> ChargeCeiling(User userToMonitor,Currency currencyToMonitor,decimal ceilingAmount);
+    }
+}
